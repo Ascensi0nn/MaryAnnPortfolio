38c99e12ab375380869e1800e361982a5bb6047d
Owen Luts <oluts47@gmail.com>
2024-02-10 12:48:47 -0500

    minor tweaks

--- scripts/add_images.js
+++ scripts/add_images.js
@@ -1,4 +1,6 @@
-function add_images(element, image_start, image_end, url) {
+const drawingList = [];
+
+function addDrawings(element, image_start, image_end, url) {
     for (let i = image_start; i <= image_end; i++) {
         const image = document.createElement('img');
         image.classList.add('image', 'drawings');
@@ -8,11 +10,19 @@ function add_images(element, image_start, image_end, url) {
                 image.classList.remove('open');
             }
             else {
+                setAllClosed();
                 image.classList.add('open');
             }
         });
+        drawingList.push(image);
         element.appendChild(image);
     }
 }
 
-add_images(document.getElementById('drawings-holder'), 1, 34, "resources/drawings/drawing");
+function setAllClosed() {
+    for (let drawing of drawingList) {
+        drawing.classList.remove('open');
+    }
+}
+
+addDrawings(document.getElementById('drawings-holder'), 1, 34, "resources/drawings/drawing");
